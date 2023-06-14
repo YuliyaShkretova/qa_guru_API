894db7014cb3c2beebe1f69c7e82a95092ee1656
YuliyaShkretova <129888062+YuliyaShkretova@users.noreply.github.com>
2023-06-14 14:36:01 +0200

    added tests

--- tests_open_api/tests_api.py
+++ tests_open_api/tests_api.py
@@ -1,47 +1,143 @@
+import json
+import time
+from pprint import pprint
+from random import randint
+
 import requests
 from module import BASE_URL
 
 
-def test_requested_page_number():
-    page = 2
-    response = requests.get(BASE_URL+'users', params={'page': page})
+def test_update_user_data_by_put():
+    name = "morpheus"
+    job = "zion resident"
 
-    assert response.status_code == 200
-    assert response.json()['page'] == page
-
-
-def test_users_list_default_length():
-    default_users_count = 6
-
-    response = requests.get(BASE_URL + 'users')
-
-    assert len(response.json()['data']) == default_users_count
-
-
-def test_single_user_not_found():
-    response = requests.get(BASE_URL + 'users/23')
-
-    assert response.status_code == 404
-    assert response.text == '{}'
-
-
-def test_create_user():
-    name = "jane"
-    job = "job"
-
-    response = requests.post(
-        url=BASE_URL+'users',
+    response = requests.put(
+        url=BASE_URL + 'users/2',
         json={
             "name": name,
             "job": job}
     )
 
+    assert response.status_code == 200
+    assert response.json()['name'] == name
+    assert response.json()['job'] == job
+    json_data = response.json()
+    assert 'updatedAt' in json_data
+    pprint(response.json())
+
+
+def test_update_user_data_patch():
+    name = "morpheus"
+    job = "zion resident"
+
+    response = requests.patch(
+        url=BASE_URL + 'users/2',
+        json={
+            "name": name,
+            "job": job}
+    )
+
+    assert response.status_code == 200
+    assert response.json()['name'] == name
+    assert response.json()['job'] == job
+    json_data = response.json()
+    assert 'updatedAt' in json_data
+    pprint(response.json())
+
+
+def test_get_full_data_of_user():
+    response = requests.get(BASE_URL + 'users/12'
+                            )
+    assert response.status_code == 200
+    assert response.json()['data']['id'] == 12
+    pprint(response.json())
+
+
+def test_user_registration_successful():
+    email = 'eve.holt@reqres.in'
+    password = 'pistol'
+    response = requests.post(BASE_URL+'register',
+        {
+            "email": email,
+            "password": password
+        }
+    )
+    assert response.status_code == 200
+    assert response.json()['id'] is not None
+    assert response.json()['token'] is not None
+    # assert response.json()['id'] == 4
+    # assert response.json()['token'] == 'QpwL5tke4Pnpja7X4'
+    pprint(response.json())
+
+
+def test_create_user_successful():
+    name = 'dfjkdfdgf'
+    job = 'dfgfghgjh'
+    response = requests.post(BASE_URL + 'users',
+                             {
+                                 "name": name,
+                                 "job": job
+                             }
+                             )
     assert response.status_code == 201
     assert response.json()['name'] == name
+    assert response.json()['job'] == job
+    assert 'createdAt' in response.json()
+    pprint(response.json())
 
 
-def test_delete_user_returns_204():
-    response = requests.delete(url=BASE_URL+'users/2')
-
+def test_delete_user():
+    response = requests.delete(BASE_URL + 'users/12')
     assert response.status_code == 204
     assert response.text == ''
+
+
+def test_get_uncreated_user():
+    user = requests.get(BASE_URL + 'unknown')
+    data = user.json()
+    user_list = [item['id'] for item in data['data']]
+    a = user_list[-1]+1
+    print(a)
+    user_id = randint(a, 100)
+    response = requests.get(BASE_URL + 'unknown/' + f'{user_id}')
+    assert response.status_code == 404
+    assert response.json() == {}
+    pprint(user_id)
+    pprint(response.json())
+
+
+def test_unsuccessful_login():
+    email = 'gcg@gfgnf.rwe'
+
+    response = requests.post(
+        url=BASE_URL + 'login',
+        json={
+            "email": email}
+    )
+    assert response.status_code == 400
+    assert response.json().get('error') == "Missing password"
+    pprint(response.json())
+
+
+def test_unsuccessful_registration():
+    email = 'gcg@gfgnf.rwe'
+
+    response = requests.post(
+        url=BASE_URL + 'register',
+        json={
+            "email": email}
+    )
+    assert response.status_code == 400
+    assert response.json().get('error') == "Missing password"
+    pprint(response.json())
+
+
+def test_delay_reply():
+    time_delay = 10
+    start_time = time.time()
+    response = requests.get(
+        BASE_URL + 'users/', params={'delay': time_delay})
+    end_time = time.time()
+    assert response.status_code == 200
+    assert (end_time - start_time) >= time_delay
+    pprint(response.json())
